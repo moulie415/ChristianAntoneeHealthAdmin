7bcb18339bc70bd42f9d2982443495e1496c0464
Henry Moule <hen10@hotmail.co.uk>
2023-02-07 20:12:10 +0000

    remove react draft wysiwyg

--- src/education.js
+++ src/education.js
@@ -24,16 +24,6 @@ import {
   RichTextField,
   required,
 } from 'react-admin';
-import {
-  EditorState,
-  convertToRaw,
-  convertFromHTML,
-  ContentState,
-} from 'draft-js';
-import {Editor} from 'react-draft-wysiwyg';
-import draftToHtml from 'draftjs-to-html';
-import htmlToDraft from 'html-to-draftjs';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import {useController} from 'react-hook-form';
 
 export const EducationList = props => (
@@ -72,39 +62,39 @@ const CustomBodyInput = () => {
 
   const {field} = useController({name: 'body'});
 
-  const getStateFromBody = () => {
-    const blocksFromHTML = convertFromHTML(body);
-    const state = ContentState.createFromBlockArray(
-      blocksFromHTML.contentBlocks,
-      blocksFromHTML.entityMap,
-    );
-    return EditorState.createWithContent(state);
-  };
+  // const getStateFromBody = () => {
+  //   const blocksFromHTML = convertFromHTML(body);
+  //   const state = ContentState.createFromBlockArray(
+  //     blocksFromHTML.contentBlocks,
+  //     blocksFromHTML.entityMap,
+  //   );
+  //   return EditorState.createWithContent(state);
+  // };
 
-  const [editorState, setEditorState] = useState(
-    body ? getStateFromBody() : EditorState.createEmpty(),
-  );
-  return (
-    <div
-      style={{
-        border: '1px solid rgba(0, 0, 0, 0.23)',
-        padding: 10,
-        borderRadius: 5,
-      }}>
-      <Editor
-        editorState={editorState}
-        wrapperClassName="demo-wrapper"
-        editorClassName="demo-editor"
-        onEditorStateChange={state => {
-          field.onChange(
-            draftToHtml(convertToRaw(editorState.getCurrentContent())),
-          );
-          setEditorState(state);
-        }}
-        placeholder="Enter content here..."
-      />
-    </div>
-  );
+  // const [editorState, setEditorState] = useState(
+  //   body ? getStateFromBody() : EditorState.createEmpty(),
+  // );
+  // return (
+  //   <div
+  //     style={{
+  //       border: '1px solid rgba(0, 0, 0, 0.23)',
+  //       padding: 10,
+  //       borderRadius: 5,
+  //     }}>
+  //     <Editor
+  //       editorState={editorState}
+  //       wrapperClassName="demo-wrapper"
+  //       editorClassName="demo-editor"
+  //       onEditorStateChange={state => {
+  //         field.onChange(
+  //           draftToHtml(convertToRaw(editorState.getCurrentContent())),
+  //         );
+  //         setEditorState(state);
+  //       }}
+  //       placeholder="Enter content here..."
+  //     />
+  //   </div>
+  // );
 };
 
 export const EducationShow = props => {
