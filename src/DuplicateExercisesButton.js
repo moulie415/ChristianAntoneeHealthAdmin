fde4d6557cea72939511774466ffecb9cfed5174
Henry Moule <henry.moule@zenobe.com>
2023-12-17 21:13:22 +0000

    fix duplication logic

--- src/DuplicateExercisesButton.js
+++ src/DuplicateExercisesButton.js
@@ -57,8 +57,8 @@ const DuplicateExercisesButton = ({source}) => {
               color="primary"
               onClick={() => {
                 handleClose();
-                const arr = exercises;
-                for (let i = 0; i < value; i++) {
+                const arr = [];
+                for (let i = 0; i <= value; i++) {
                   arr.push(...exercises);
                 }
                 context.setValue(source, arr);
