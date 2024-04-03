374093a6e97653d365363372d73df1bb0bfadfef
Henry Moule <henry.moule@zenobe.com>
2024-04-03 22:52:41 +0100

    pagination changes

--- src/common/PremiumField.tsx
+++ src/common/PremiumField.tsx
@@ -3,9 +3,6 @@ import {useRecordContext} from 'react-admin';
 
 const PremiumField: React.FC<{source: string}> = props => {
   const record = useRecordContext(props);
-  if (record?.email === 'henry.moule@gmail.com') {
-    console.log(record);
-  }
   return (
     <span>
       {record?.premium && Object.keys(record.premium)?.[0] ? (
--- src/users/UsersList.tsx
+++ src/users/UsersList.tsx
@@ -4,6 +4,7 @@ import DoneIcon from '@mui/icons-material/Done';
 import {
   Button,
   CircularProgress,
+  Input,
   Paper,
   Table,
   TableBody,
@@ -14,9 +15,17 @@ import {
   ToggleButton,
   ToggleButtonGroup,
 } from '@mui/material';
-import {collection, getDocs, limit, query, where} from 'firebase/firestore';
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  where,
+} from 'firebase/firestore';
+import * as _ from 'lodash';
 import moment from 'moment';
-import {useEffect, useState} from 'react';
+import {useCallback, useEffect, useRef, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 import {toast} from 'react-toastify';
 import {db} from '../App';
@@ -30,7 +39,6 @@ const PremiumField: React.FC<{user: any}> = ({user}) => {
   const userPremium = getUserPremium(user);
   const hasExpired =
     !!userPremium && moment(userPremium.expirationDate).isBefore(moment());
-  console.log(userPremium);
   return (
     <div>
       {userPremium ? (
@@ -55,112 +63,178 @@ type PremiumToggle = 'premium' | 'premiumPlus';
 
 const UsersList = () => {
   const [loading, setLoading] = useState(false);
-  const [users, setUsers] = useState<any[]>();
+  const [users, setUsers] = useState<any[]>([]);
   const [marketing, setMarketing] = useState<boolean>();
+  const [emailFilter, setEmailFilter] = useState('');
+
+  const tableRef = useRef<HTMLDivElement>(null);
 
   const [premiumToggle, setPremiumToggle] = useState<PremiumToggle>();
 
-  useEffect(() => {
-    const getUsers = async () => {
-      try {
-        setLoading(true);
-        const conditions = [];
-        if (premiumToggle === 'premiumPlus' || premiumToggle === 'premium') {
-          conditions.push(where('premium', '!=', false));
-        }
-        if (marketing !== undefined) {
-          conditions.push(where('marketing', '==', marketing));
-        }
-        const userQuery = query(
-          collection(db, 'users'),
-          ...conditions,
-          limit(200),
+  const cursor = useRef(0);
+
+  const getUsers = useCallback(async () => {
+    try {
+      setLoading(true);
+      console.log('fetching');
+      const conditions = [];
+      if (premiumToggle === 'premiumPlus' || premiumToggle === 'premium') {
+        conditions.push(where('premium', '!=', false));
+      }
+      if (marketing !== undefined && marketing !== null) {
+        conditions.push(where('marketing', '==', marketing));
+      }
+
+      if (cursor.current) {
+        conditions.push(where('signUpDate', '<', cursor.current));
+      }
+
+      const userQuery = query(
+        collection(db, 'users'),
+        ...conditions,
+        orderBy('signUpDate', 'desc'),
+        limit(100),
+      );
+
+      const c = await getDocs(userQuery);
+      const userUids = c.docs
+        .filter(doc => getUserPremium(doc.data() as Profile))
+        .map(doc => doc.data().uid);
+      const uidArrays = chunkArrayInGroups(userUids, 10);
+      const plans: Plan[] = [];
+      for (let i = 0; i < uidArrays.length; i++) {
+        const arr = uidArrays[i];
+        const planQuery = query(
+          collection(db, 'plans'),
+          where('user', 'in', arr),
         );
-
-        const c = await getDocs(userQuery);
-        const userUids = c.docs
-          .filter(doc => getUserPremium(doc.data() as Profile))
-          .map(doc => doc.data().uid);
-        const uidArrays = chunkArrayInGroups(userUids, 10);
-        const plans: Plan[] = [];
-        for (let i = 0; i < uidArrays.length; i++) {
-          const arr = uidArrays[i];
-          const planQuery = query(
-            collection(db, 'plans'),
-            where('user', 'in', arr),
-          );
-          const snapshot = await getDocs(planQuery);
-          plans.push(...(snapshot.docs.map(doc => doc.data()) as Plan[]));
-        }
-
-        setUsers(
-          c.docs
-            .filter(doc => {
-              const user = doc.data() as Profile;
-              const premium = getUserPremium(user);
-              const hasExpired =
-                !!premium && moment(premium.expirationDate).isBefore(moment());
-              if (
-                premiumToggle === 'premiumPlus' &&
-                (premium?.identifier !== 'Premium Plus' || hasExpired)
-              ) {
-                return false;
-              }
-              if (
-                premiumToggle === 'premium' &&
-                (premium?.identifier !== 'Premium' || hasExpired)
-              ) {
-                return false;
-              }
-              return true;
-            })
-            .map(doc => {
-              const data = doc.data();
-
-              const userPlans = plans.filter(plan => plan.user === data.uid);
-              const upToDatePlan = userPlans?.find(plan => {
-                return plan?.workouts?.some(workout => {
-                  return workout.dates?.some(date =>
-                    moment(date).isSameOrAfter(moment(), 'day'),
-                  );
-                });
-              });
-              return {...data, plans: userPlans, upToDatePlan};
-            }),
-        );
-      } catch (e) {
-        if (e instanceof Error) {
-          toast.error('Error fetching users: ' + e.message);
-        } else {
-          toast.error('Error fetching users');
-        }
+        const snapshot = await getDocs(planQuery);
+        plans.push(...(snapshot.docs.map(doc => doc.data()) as Plan[]));
       }
       setLoading(false);
-    };
-    getUsers();
+      return c.docs
+        .filter(doc => {
+          const user = doc.data() as Profile;
+          const premium = getUserPremium(user);
+          const hasExpired =
+            !!premium && moment(premium.expirationDate).isBefore(moment());
+          if (
+            premiumToggle === 'premiumPlus' &&
+            (premium?.identifier !== 'Premium Plus' || hasExpired)
+          ) {
+            return false;
+          }
+          if (
+            premiumToggle === 'premium' &&
+            (premium?.identifier !== 'Premium' || hasExpired)
+          ) {
+            return false;
+          }
+          return true;
+        })
+        .map(doc => {
+          const data = doc.data();
+
+          const userPlans = plans.filter(plan => plan.user === data.uid);
+          const upToDatePlan = userPlans?.find(plan => {
+            return plan?.workouts?.some(workout => {
+              return workout.dates?.some(date =>
+                moment(date).isSameOrAfter(moment(), 'day'),
+              );
+            });
+          });
+          return {...data, plans: userPlans, upToDatePlan};
+        });
+    } catch (e) {
+      if (e instanceof Error) {
+        toast.error('Error fetching users: ' + e.message);
+      } else {
+        toast.error('Error fetching users');
+      }
+    }
+    setLoading(false);
   }, [marketing, premiumToggle]);
+
+  useEffect(() => {
+    const init = async () => {
+      const users = await getUsers();
+      if (users) {
+        setUsers(users);
+      }
+    };
+    init();
+  }, [getUsers]);
 
   const navigate = useNavigate();
 
-  const handleChange = (
+  const handlePremiumToggleChange = (
     event: React.MouseEvent<HTMLElement>,
     toggle: PremiumToggle,
   ) => {
     setPremiumToggle(toggle);
   };
 
+  const handleMarketingToggleChange = (
+    event: React.MouseEvent<HTMLElement>,
+    toggle: boolean,
+  ) => {
+    setMarketing(toggle);
+  };
+
+  const handleEmailFilterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmailFilter(e.target.value);
+  };
+
+  const handleScroll = async () => {
+    const table = tableRef.current;
+    if (table) {
+      const isAtBottom =
+        table.scrollTop + table.clientHeight >= table.scrollHeight - 5;
+      if (isAtBottom) {
+        const newCursor = _.minBy(users, 'signUpDate')?.signUpDate;
+        if (newCursor !== cursor.current) {
+          cursor.current = newCursor;
+          const newUsers = await getUsers();
+          if (newUsers) {
+            setUsers([...users, ...newUsers]);
+          }
+        }
+      }
+    }
+  };
+
   return (
     <div>
-      <ToggleButtonGroup
-        style={{marginTop: 50, marginBottom: 10}}
-        color="primary"
-        value={premiumToggle}
-        exclusive
-        onChange={handleChange}
-        aria-label="Platform">
-        <ToggleButton value="premium">Premium</ToggleButton>
-        <ToggleButton value="premiumPlus">Premium Plus</ToggleButton>
-      </ToggleButtonGroup>
+      <div style={{marginBottom: 10, marginTop: 50}}>
+        <ToggleButtonGroup
+          style={{marginRight: 10}}
+          disabled={loading}
+          color="primary"
+          value={premiumToggle}
+          exclusive
+          onChange={handlePremiumToggleChange}
+          aria-label="Platform">
+          <ToggleButton value="premium">Premium</ToggleButton>
+          <ToggleButton value="premiumPlus">Premium Plus</ToggleButton>
+        </ToggleButtonGroup>
+        <ToggleButtonGroup
+          color="primary"
+          disabled={loading}
+          value={marketing}
+          exclusive
+          onChange={handleMarketingToggleChange}
+          aria-label="Platform">
+          <ToggleButton value={true}>Marketing ON</ToggleButton>
+          <ToggleButton value={false}>Marketing OFF</ToggleButton>
+        </ToggleButtonGroup>
+        <Input
+          value={emailFilter}
+          disabled={loading}
+          style={{marginLeft: 20, width: 200}}
+          placeholder="Filter via email"
+          onChange={handleEmailFilterChange}
+        />
+      </div>
       {loading ? (
         <div
           style={{
@@ -172,7 +246,11 @@ const UsersList = () => {
           <CircularProgress />
         </div>
       ) : (
-        <TableContainer component={Paper}>
+        <TableContainer
+          onScroll={handleScroll}
+          style={{height: '80vh'}}
+          ref={tableRef}
+          component={Paper}>
           <Table sx={{minWidth: 700}} aria-label="customized table">
             <TableHead>
               <TableRow>
@@ -186,6 +264,13 @@ const UsersList = () => {
             </TableHead>
             <TableBody>
               {users
+                ?.filter(
+                  user =>
+                    !emailFilter ||
+                    user.email
+                      .toLowerCase()
+                      .includes(emailFilter.toLowerCase()),
+                )
                 ?.sort((a, b) => {
                   const aPremium = getUserPremium(a);
                   const bPremium = getUserPremium(b);
