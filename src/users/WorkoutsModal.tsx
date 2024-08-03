ceefb565d042ab91cd409a8f49001026a73fae2e
Henry Moule <henry.moule@zenobe.com>
2024-08-03 20:15:45 +0100

    fix: Workout chart fixes

--- src/users/WorkoutsModal.tsx
+++ src/users/WorkoutsModal.tsx
@@ -1,44 +1,51 @@
 import {Box, CircularProgress, Fade, Modal} from '@mui/material';
 import moment from 'moment';
+import React from 'react';
 import {
   CartesianGrid,
   Label,
+  Legend,
   Line,
   LineChart,
-  ReferenceLine,
   ResponsiveContainer,
   Tooltip,
   XAxis,
   YAxis,
 } from 'recharts';
 import {SavedQuickRoutine, SavedWorkout} from '../types/Shared';
+import colors from '../colors';
 
 const WorkoutsModal: React.FC<{
   handleClose: () => void;
   open: boolean;
   selectedWorkout?: SavedWorkout | SavedQuickRoutine;
-  data?: (SavedWorkout | SavedQuickRoutine)[];
   isPending: boolean;
 }> = ({open, handleClose, isPending, selectedWorkout}) => {
   if (!selectedWorkout) {
     return null;
   }
 
-  console.log(selectedWorkout);
-  const {
-    heartRateSamples,
-    calories,
-    averageHeartRate,
-    calorieSamples,
-    difficulty,
-    exerciseEvents,
-  } = selectedWorkout;
+  const {heartRateSamples, calorieSamples, exerciseEvents, pauseEvents} =
+    selectedWorkout;
 
+  // Convert heart rate samples to seconds
   const heartRateSamplesInSeconds = heartRateSamples.map(sample => ({
     ...sample,
     startDate: moment(sample.startDate).unix(),
     endDate: moment(sample.startDate).unix(),
   }));
+
+  // Calculate cumulative calories
+  let cumulativeCalories = 0;
+  const calorieSamplesInSeconds = calorieSamples.map(sample => {
+    cumulativeCalories += sample.value;
+    return {
+      ...sample,
+      startDate: moment(sample.startDate).unix(),
+      endDate: moment(sample.startDate).unix(),
+      cumulativeValue: cumulativeCalories,
+    };
+  });
 
   return (
     <Modal
@@ -46,14 +53,7 @@ const WorkoutsModal: React.FC<{
       aria-describedby="transition-modal-description"
       open={open}
       onClose={handleClose}
-      closeAfterTransition
-      // slots={{backdrop: Backdrop}}
-      // slotProps={{
-      //   backdrop: {
-      //     timeout: 500,
-      //   },
-      // }}
-    >
+      closeAfterTransition>
       <Fade in={open}>
         <Box
           sx={{
@@ -71,42 +71,101 @@ const WorkoutsModal: React.FC<{
             <CircularProgress />
           ) : (
             <ResponsiveContainer height={720}>
-              <LineChart margin={{bottom: 20}} data={heartRateSamplesInSeconds}>
-                <YAxis>
-                  <Label value="Heart rate (bpm)" angle={270} dx={-20} />
-                </YAxis>
+              <LineChart margin={{bottom: 20}}>
+                <YAxis
+                  yAxisId="left"
+                  label={{
+                    value: 'Heart rate (bpm)',
+                    angle: -90,
+                    position: 'insideLeft',
+                  }}
+                />
+                <YAxis
+                  yAxisId="right"
+                  orientation="right"
+                  label={{
+                    value: 'Calories Burnt',
+                    angle: 90,
+                    position: 'insideRight',
+                  }}
+                />
+
                 <XAxis
                   minTickGap={120}
-                  tickFormatter={unixTime => {
-                    return moment.unix(unixTime).format('HH:mm');
-                  }}
+                  xAxisId="right"
+                  hide
+                  tickFormatter={unixTime =>
+                    moment.unix(unixTime).format('HH:mm')
+                  }
+                  dataKey="startDate">
+                  <Label value="Time" dy={20} />
+                </XAxis>
+                <XAxis
+                  xAxisId="left"
+                  minTickGap={120}
+                  tickFormatter={unixTime =>
+                    moment.unix(unixTime).format('HH:mm')
+                  }
                   dataKey="startDate">
                   <Label value="Time" dy={20} />
                 </XAxis>
                 <Line
+                  yAxisId="left"
+                  xAxisId="left"
                   type="monotone"
                   dataKey="value"
-                  stroke="red"
+                  stroke={colors.appRed}
                   dot={false}
+                  data={heartRateSamplesInSeconds}
                 />
-
+                <Line
+                  yAxisId="right"
+                  xAxisId="right"
+                  type="monotone"
+                  dataKey="cumulativeValue"
+                  stroke={colors.secondaryDark}
+                  dot={false}
+                  data={calorieSamplesInSeconds}
+                />
                 <Tooltip
-                  formatter={value => [`${value} bpm`, 'Heart rate']}
-                  labelFormatter={unixTime => {
-                    return moment.unix(unixTime).format('HH:mm');
-                  }}
+                  formatter={(value, name) =>
+                    name === 'cumulativeValue'
+                      ? [`${Math.round(Number(value))} cal`, 'Calories Burnt']
+                      : [`${Math.round(Number(value))} bpm`, 'Heart rate']
+                  }
+                  labelFormatter={unixTime =>
+                    moment.unix(unixTime).format('HH:mm')
+                  }
                 />
                 <CartesianGrid stroke="#ccc" />
-                {exerciseEvents.map(event => {
+                {/* {exerciseEvents.map(event => {
+                  const eventTimeInSeconds = moment(event.time.toDate()).unix(); // Convert Firestore timestamp to Unix time in seconds
                   return (
                     <ReferenceLine
-                      stroke="blue"
-                      key={`${event?.time.toString()} ${event.value}`}
-                      x={event?.time?.seconds}>
-                      <Label value={event.value} position="insideBottomRight" />
+                      key={`exercise-${eventTimeInSeconds}`}
+                      x={eventTimeInSeconds}
+                      stroke="blue">
+                      <Label
+                        value={`Exercise: ${event.value}`}
+                        position="insideBottomRight"
+                      />
                     </ReferenceLine>
                   );
                 })}
+                {pauseEvents.map(event => {
+                  const eventTimeInSeconds = moment(event.time.toDate()).unix(); // Convert Firestore timestamp to Unix time in seconds
+                  return (
+                    <ReferenceLine
+                      key={`pause-${eventTimeInSeconds}`}
+                      x={eventTimeInSeconds}
+                      stroke="orange">
+                      <Label
+                        value={`Pause: ${event.paused ? 'Paused' : 'Resumed'}`}
+                        position="insideBottomRight"
+                      />
+                    </ReferenceLine>
+                  );
+                })} */}
               </LineChart>
             </ResponsiveContainer>
           )}
